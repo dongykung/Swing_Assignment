d33551f90d2a147f799b284836840e2de45e77a7
dongkyung <kdkd422@naver.com>
2024-04-29 21:18:06 +0900

    메인 엑티비티

---
+++ app/src/main/java/com/dkproject/swingassignment/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.dkproject.swingassignment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dkproject.swingassignment.navigation.HomeNavigation
+import com.dkproject.swingassignment.ui.theme.SwingAssignmentTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.HiltAndroidApp
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SwingAssignmentTheme {
+               HomeNavigation()
+            }
+        }
+    }
+}

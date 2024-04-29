5554924f098d39388fb69a89ce666fb6d4495d06
dongkyung <kdkd422@naver.com>
2024-04-29 21:14:51 +0900

    Constants.kt

---
+++ app/src/main/java/com/dkproject/swingassignment/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.dkproject.swingassignment.util
+
+object Constants {
+    const val START_PAGE_INDEX = 1
+    const val LOAD_PAGE_SIZE = 30
+    const val LOAD_USER_LIKE = 10
+    const val INIT_PAGE_SIZE=30
+    const val YOUSER_NAME="dongkyung"
+}

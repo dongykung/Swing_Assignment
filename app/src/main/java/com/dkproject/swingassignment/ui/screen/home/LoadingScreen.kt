4a8d1b076a9d4dcdba22a5596854957ddcab7d9d
dongkyung <kdkd422@naver.com>
2024-04-29 21:17:41 +0900

    LoadingScreen.kt

---
+++ app/src/main/java/com/dkproject/swingassignment/ui/screen/home/LoadingScreen.kt
@@ -0,0 +1,20 @@
+package com.dkproject.swingassignment.ui.screen.home
+
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Log.d("LoadingScreen", "LoadingScreen: ")
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        CircularProgressIndicator(modifier = modifier)
+    }
+}
